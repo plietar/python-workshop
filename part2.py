6eecea398fbb23d36c07897c133e87778f2e4a26
Paul Lietar <pl2113@ic.ac.uk>
2024-07-18 11:56:28 +0100

    Update

--- part2.py
+++ part2.py
@@ -1,11 +1,11 @@
-from random import random, uniform
-from collections import namedtuple
+import random
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
 
-P_INFECTION = 0.2
+P_INFECTION = 0.1
 P_RECOVERY_YOUNG = 0.02
 P_RECOVERY_OLD = 0.01
 
-State = namedtuple("State", ["t", "S", "I", "R"])
 
 class Person:
     def __init__(self, state, age):
@@ -14,16 +14,17 @@ class Person:
 
     def update(self, foi):
         if self.state == "S":
-            if random() < foi:
+            if random.random() < foi * P_INFECTION:
                 self.state = "I"
 
         elif self.state == "I":
-            if random() < self.recovery_probability():
+            if random.random() < self.recovery_probability:
                 self.state = "R"
 
         elif self.state == "R":
             pass
 
+    @property
     def recovery_probability(self):
         if self.age < 50:
             return P_RECOVERY_YOUNG
@@ -33,37 +34,57 @@ class Person:
     def __repr__(self):
         return f"Person(state={self.state}, age={self.age})"
 
+
+@dataclass
+class DataPoint:
+    S: int
+    I: int
+    R: int
+
+
 class Model:
     def __init__(self, size, infected):
         self.population = list()
         self.size = size
 
         for i in range(infected):
-            self.population.append(Person("I", uniform(0, 100)))
+            self.population.append(Person("I", random.uniform(0, 100)))
         for i in range(size - infected):
-            self.population.append(Person("S", uniform(0, 100)))
+            self.population.append(Person("S", random.uniform(0, 100)))
 
     def update(self):
-        foi = P_INFECTION * sum([1 for p in self.population if p.state == "I"]) / self.size
+        foi = self.I / self.size
 
         for p in self.population:
             p.update(foi)
 
-    def count(self, state):
-        return sum([1 for p in self.population if p.state == state])
-
     def run(self, n):
         data = []
-        for t in range(n):
+        for _ in range(n):
             self.update()
-            data.append(State(t, self.count('S'), self.count('I'), self.count('R')))
+            data.append(DataPoint(self.S, self.I, self.R))
 
         return data
 
-m = Model(100, 5)
-data = m.run(100)
+    @property
+    def S(self):
+        return self.count("S")
 
-import matplotlib.pyplot as plt
+    @property
+    def I(self):
+        return self.count("I")
+
+    @property
+    def R(self):
+        return self.count("R")
+
+    def count(self, state):
+        return sum([1 for p in self.population if p.state == state])
+
+
+m = Model(100, 5)
+data = m.run(200)
+
 plt.figure()
 plt.plot([x.S for x in data], label="S")
 plt.plot([x.I for x in data], label="I")
